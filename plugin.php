5733ac66b35d7eaf5d052b39df9f58abd62810cb
Kaede Fujisaki <kaede@hexe.net>
2022-10-08 21:52:02 +0900

    init

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: wp-long-login-cookie
+ * Description: long login
+ * Author: Kaede Fujisaki
+ * Author URI: https://7io.org
+ * Version: 1.0.0
+ *
+ */
+
+if( !function_exists( 'long_login_cookie' ) ) {
+  function long_login_cookie() {  
+    return 60 * 60 * 24 * 365 * 10;
+  }  
+  add_filter( 'auth_cookie_expiration', 'long_login_cookie' );  
+}
+
